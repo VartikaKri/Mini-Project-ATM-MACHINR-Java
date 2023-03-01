d62af5d94380fe72b542da82b29c68075912126a
VartikaKri <123318905+VartikaKri@users.noreply.github.com>
2023-03-02 00:23:52 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,16 @@
+package io.github.vartika.corejava.project.atm;
+
+import java.io.IOException;
+
+public class ATM {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		introduction();
+		optionMenu.mainMenu();
+	}
+
+	public static void introduction() {
+		System.out.println("Welcome to the ATM Project!");
+	}
+}
